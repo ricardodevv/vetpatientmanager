b7aa788ad6038b5c5cc74245f16624848e4d1197
Ricardo Quintero <ricardoaqf@Ricardos-MacBook-Pro.local>
2023-12-01 13:58:17 +0100

    v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
 
   useEffect(() => {
     const getLocalStorage = () => {
-      const patients = JSON.parse(localStorage.getItem("patients") || "") ?? [];
+      const patients = JSON.parse(localStorage.getItem("patients")!) ?? [];
       setPatients(patients);
     };
     getLocalStorage();
